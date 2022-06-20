9a61ad7b7a056adcc813eec0e4c6574e9025b3c4
neplik1 <neplik1@gmail.com>
2022-06-20 11:23:30 +0300

    created fun startTimer

--- app/src/main/java/com/example/composition/presintation/GameViewModel.kt
+++ app/src/main/java/com/example/composition/presintation/GameViewModel.kt
@@ -1,5 +1,8 @@
 package com.example.composition.presintation
 
+import android.os.CountDownTimer
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.composition.data.GameRepositoriImpl
 import com.example.composition.domain.entity.GameSettings
@@ -16,8 +19,49 @@ class GameViewModel : ViewModel() {
     private val generateQuestionUseCase = GenerateQuestionUseCase(repository)
     private val getGameSettingsUseCase = GetGameSettingsUseCase(repository)
 
-    fun StartGame(level: Level){
+    private val _formattedTime = MutableLiveData<String>()
+    val formattedTime: LiveData<String>
+        get() = _formattedTime
+
+
+    fun startGame(level: Level) {
+        getGameSettings(level)
+        startTimer()
+    }
+
+    private fun getGameSettings(level: Level) {
         this.level = level
-        this.gameSettings =getGameSettingsUseCase(level)
+        this.gameSettings = getGameSettingsUseCase(level)
+    }
+
+    private fun startTimer() {
+        val timer = object : CountDownTimer(
+            gameSettings.gameTimeSeconds * MILLIS_IN_SECONDS,
+            MILLIS_IN_SECONDS
+        ) {
+            override fun onTick(p0: Long) {
+                _formattedTime.value = formatTime(p0)
+            }
+
+            override fun onFinish() {
+                finishGame()
+            }
+        }
+    }
+
+    fun formatTime(p0: Long): String {
+        val seconds = p0 / MILLIS_IN_SECONDS
+        val minutes = seconds / SECONDS_IN_MINUTES
+        val leftSeconds = seconds - (minutes * SECONDS_IN_MINUTES)
+        return String.format("%02d:%02d", minutes, leftSeconds)
+
+    }
+
+    private fun finishGame() {
+    }
+
+    companion object {
+        private const val MILLIS_IN_SECONDS = 1000L
+        private const val SECONDS_IN_MINUTES = 60
     }
 }
